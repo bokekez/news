6c6e0f86ec2428c56db98d19c8f25e3dfd6d5669
bokekez <bokekez@gmail.com>
2024-12-06 11:44:26 +0100

    cat images

--- client/src/components/Categories/Categories.tsx
+++ client/src/components/Categories/Categories.tsx
@@ -1,6 +1,14 @@
 import React, { useContext, useState, useEffect} from 'react';
 import styles from './Categories.module.scss';
 import { AuthContext } from '../../context/authContext';
+import Home from '../../../resources/categories/home.png'
+import General from '../../../resources/categories/news.png'
+import Business from '../../../resources/categories/business.png'
+import Health from '../../../resources/categories/health.png'
+import Science from '../../../resources/categories/science.png'
+import Sports from '../../../resources/categories/sports.png'
+import Technology from '../../../resources/categories/tech.png'
+import Favorites from '../../../resources/favorite.png'
 
 interface CategorySidebarProps {
   onCategorySelect: (category: string) => void;
@@ -11,37 +19,40 @@ const CategorySidebar: React.FC<CategorySidebarProps> = ({
   onCategorySelect,
   activeCategory,
 }) => {
-  const [categories, setCategories] = useState<string[]>([
-    'Home',
-    'General',
-    'Business',
-    'Health',
-    'Science',
-    'Sport',
-    'Technology'
+  const [categories, setCategories] = useState<object[]>([
+    {name: 'Home', img: Home},
+    {name: 'General', img: General},
+    {name: 'Business', img: Business},
+    {name: 'Health', img: Health},
+    {name: 'Science', img: Science},
+    {name: 'Sports', img: Sports},
+    {name: 'Technology', img: Technology},
   ])
   const authContext = useContext(AuthContext);
 
   useEffect(() => {
-    if (authContext?.user?.id && !categories.includes('Favorites')) {
-      setCategories((prevCategories) => [...prevCategories, 'Favorites']);
+    if (authContext?.user?.id && !categories.some(cat => cat.name === 'Favorites')) {
+      setCategories((prevCategories) => [...prevCategories, {name: 'Favorites', img:Favorites}]);
     }
-    if (!authContext?.user?.id && categories.includes('Favorites')) {
-      setCategories(categories.filter(category => category !== 'Favorites'));
+    if (!authContext?.user?.id && categories.some(cat => cat.name === 'Favorites')) {
+      setCategories(categories.filter(category => category.name !== Favorites));
     }
   }, [authContext?.user?.id, categories]);
+
+  console.log(categories)
 
   return (
     <div className={styles.sidebar}>
       {categories.map((category) => (
         <button
-          key={category}
+          key={category.name}
           className={`${styles.categoryButton} ${
-            activeCategory === category ? styles.active : ''
+            activeCategory === category.name ? styles.active : ''
           }`}
-          onClick={() => onCategorySelect(category.toLowerCase())}
+          onClick={() => onCategorySelect(category.name.toLowerCase())}
         >
-          {category}
+          <img src={category.img}/>
+          {category.name}
         </button>
       ))}
     </div>
